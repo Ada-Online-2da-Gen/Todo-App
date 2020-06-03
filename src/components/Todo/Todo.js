1eefc8e123cf56189157f9589396eba48523f432
Josefina del Rey <delreyjo@gmail.com>
2020-06-03 15:05:22 -0300

    modifico nombre de la variable del useState, visible por isMouseOver

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -3,14 +3,14 @@ import React, { useState } from 'react'
 import ListItem from 'components/ListItem/ListItem'
 
 const Todo = ({ icon, children, ...props }) => {
-  const [visible, setVisible] = useState(false)
-  const handleMouseEnter = () => setVisible(true)
+  const [isMouseOver, setIsMouseOver] = useState(false)
+  const handleMouseEnter = () => setIsMouseOver(true)
 
-  const handleMouseLeave = () => setVisible(false)
+  const handleMouseLeave = () => setIsMouseOver(false)
   return (
     <ListItem {...props} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       {children}
-      {visible && icon}
+      {isMouseOver && icon}
     </ListItem>
   )
 }
